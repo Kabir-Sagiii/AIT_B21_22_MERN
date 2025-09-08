0e62074b32c2666fe6e1cb3d8392072353e74a9b
Kabir <sagithedeveloper@gmail.com>
2025-09-08 08:24:20 +0530

    Added the code

---
+++ 03_Javascript/S04_Variables_Data-types/Variables.js
@@ -0,0 +1,19 @@
+var id;
+var username;
+var city;
+
+id = 101;
+username = "Kabir";
+city = "hyd";
+
+var pin = 12345;
+var price = 5678;
+var productName = "Samsung Galaxy S25 ultra";
+
+var owner = undefined;
+
+var currentstate = null;
+
+currentstate = 101;
+
+currentstate = "MH";

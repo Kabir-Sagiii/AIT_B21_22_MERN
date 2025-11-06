9975363373b286d136ce0f600347223dd516a7c5
Kabir <sagithedeveloper@gmail.com>
2025-11-06 09:31:14 +0530

    Added

---
+++ 04_React JS/S13/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import Navbar from "./components/nav-bar/Navbar";
+import AllCategories from "./components/all-categories/AllCategories";
+import Ad from "./components/ad/Ad";
+import Footer from "./components/footer/Footer.jsx";
+import Title from "./components/state/Title.jsx";
+import NewTitle from "./components/state/NewTitle.jsx";
+import Task1 from "./components/state/Task1.jsx";
+import Image from "./components/state/Image.jsx";
+import Counter from "./components/state/Counter.jsx";
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Counter />
+      <Image />
+    </div>
+  );
+}
+
+export default App;

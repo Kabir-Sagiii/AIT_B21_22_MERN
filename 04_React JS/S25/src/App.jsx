8fdd9f5be17106ee78feafa37ac0e72fd9dbfd22
Kabir <sagithedeveloper@gmail.com>
2025-11-26 09:24:11 +0530

    Added the code

---
+++ 04_React JS/S25/src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+
+import Navbar from "./components/nav-bar/Navbar";
+import Profile from "./pages/profile/Profile.jsx";
+import Signin from "./pages/sign-in/Signin.jsx";
+import Signup from "./pages/sign-up/Signup.jsx";
+import Home from "./pages/home/Home.jsx";
+import Products from "./pages/products/Products.jsx";
+import { Route, Routes } from "react-router-dom";
+import Ref from "./components/use-ref/Ref.jsx";
+import DomRef from "./components/use-ref/DomRef.jsx";
+import UseReducerComp from "./components/use-reducer/UseReducerComp.jsx";
+import ProductDetails from "./pages/product-details/Product-Details.jsx";
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      {/* <UseReducerComp />
+      <Ref />
+      <DomRef /> */}
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/products"} element={<Products />} />
+        <Route path={"/profile"} element={<Profile />} />
+        <Route path={"/signin"} element={<Signin />} />
+        <Route path={"/signup"} element={<Signup />} />
+        <Route path={"/productdetails"} element={<ProductDetails />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
